a9f71f7eba04d2354d8d2d51a2317fddfb5d9ea6
minecraft_20200 <76248694+galaxydeveloperz@users.noreply.github.com>
2021-11-14 17:27:16 +0000

    Update initialBackground.js

--- initialBackground.js
+++ initialBackground.js
@@ -1,1 +1,15 @@
-if(localStorage.getItem("nightTabStyle")){const e=document.createElement("style");switch(e.type="text/css",e.media="screen",localStorage.getItem("nightTabStyle")){case"light":e.innerHTML="html, body {background-color: rgb(255, 255, 255);}";break;case"dark":e.innerHTML="html, body {background-color: rgb(0, 0, 0);}"}document.querySelector("head").appendChild(e)}
+if (localStorage.getItem("nightTabStyle")) {
+  const e = document.createElement("style");
+  switch (
+    ((e.type = "text/css"),
+    (e.media = "screen"),
+    localStorage.getItem("nightTabStyle"))
+  ) {
+    case "light":
+      e.innerHTML = "html, body {background-color: rgb(255, 255, 255);}";
+      break;
+    case "dark":
+      e.innerHTML = "html, body {background-color: rgb(0, 0, 0);}";
+  }
+  document.querySelector("head").appendChild(e);
+}
